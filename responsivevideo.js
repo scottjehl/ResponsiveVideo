1e66254182a141c9e3ce5f6dd2510e4271767d13
Scott Jehl <scottjehl@gmail.com>
2023-12-12 09:43:07 -0500

    add feature test

--- responsivevideo.js
+++ responsivevideo.js
@@ -12,36 +12,31 @@ class ResponsiveVideo extends HTMLElement {
     this.listenedMedia = [];
     this.reloadQueued = false;
   }
-
   connectedCallback() {
     this.bindMediaListeners();
   }
-
   disconnectedCallback() {
     this.unbindMediaListeners();
   }
-
   bindMediaListeners(){
     const video = this.video;
     this.querySelectorAll('source').forEach( source => {
-      if( source.media ){
-        let mqListener = () => {
-            if( source.media === video.currentSrc || !this.previousSiblingIsPlaying(source, video.currentSrc) && !this.reloadQueued ){
-                this.reloadVideo();
-            }
-        };
-        this.listenedMedia.push({media: source.media, handler: mqListener});
-        window.matchMedia( source.media ).addEventListener("change", mqListener);
-      }
+    if( source.media ){
+      let mqListener = () => {
+        if( source.media === video.currentSrc || !this.previousSiblingIsPlaying(source, video.currentSrc) && !this.reloadQueued ){
+          this.reloadVideo();
+        }
+      };
+      this.listenedMedia.push({media: source.media, handler: mqListener});
+      window.matchMedia( source.media ).addEventListener("change", mqListener);
+    }
     }, this );
   }
-
   unbindMediaListeners(){
     this.listenedMedia.forEach( listener => {
-      window.matchMedia( listener.media ).removeEventListener("change", listener.handler);
+    window.matchMedia( listener.media ).removeEventListener("change", listener.handler);
     });
   }
-
   previousSiblingIsPlaying(elem, src) {
     let prevSibling = elem;
     let ret = false;
@@ -52,20 +47,44 @@ class ResponsiveVideo extends HTMLElement {
     }
     return ret;
   }
-
   reloadVideo(){
     this.reloadQueued = true;
     const currentTime = this.video.currentTime;
     const playState = this.video.playState;
     this.video.load();
     let videoLoaded = () => {
-        this.video.playState = playState;
-        this.video.currentTime = currentTime.toString();
-        this.reloadQueued = false;
-        this.video.removeEventListener("loadeddata", videoLoaded);
+      this.video.playState = playState;
+      this.video.currentTime = currentTime.toString();
+      this.reloadQueued = false;
+      this.video.removeEventListener("loadeddata", videoLoaded);
     }
     this.video.addEventListener("loadeddata", videoLoaded );
   }
 }
 
-customElements.define("responsive-video", ResponsiveVideo);
+// feature test for native video media switching media
+const videoMediaChangeSupport = async () => {
+  return new Promise((resolve) => {
+    const iframe = document.createElement("iframe");
+    const video = document.createElement("video");
+    const source = document.createElement("source");
+    const mediaSource = new MediaSource();
+    mediaSource.addEventListener("sourceopen", () => resolve(true));
+    source.src = URL.createObjectURL(mediaSource);
+    source.media = "(min-width:10px)";
+    video.append(source);
+    iframe.width = "5";
+    iframe.style.cssText = `position: absolute; visibility: hidden;`;
+    document.documentElement.append(iframe);
+    iframe.contentDocument.body.append(video);
+    setTimeout(() => { iframe.width = "15px"; });
+    setTimeout(() => {
+      resolve(false);
+      iframe.remove();
+    }, 1000);
+  });
+};
+
+if( await videoMediaChangeSupport() === false ){
+  customElements.define("responsive-video", ResponsiveVideo);
+}
