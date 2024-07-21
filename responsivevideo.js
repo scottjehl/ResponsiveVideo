f0f2b8547139d9b215f66009b727d779e32fda64
Scott Jehl <scottjehl@users.noreply.github.com>
2024-07-21 14:27:07 -0500

    audio just for kicks

--- responsivevideo.js
+++ responsivevideo.js
@@ -1,18 +1,18 @@
 /*
-ResponsiveVideo Web Component
-This makes a responsive video element respond to source media changes too
-To use: load this module in your page and wrap a responsive-video element around your video element
+ResponsiveAudio Web Component
+This makes a responsive audio element respond to source media changes too
+To use: load this module in your page and wrap a responsive-audio element around your audio element
 Copyright 2023 Scott Jehl, @scottjehl
 MIT License
 */
-class ResponsiveVideo extends HTMLElement {
+class ResponsiveAudio extends HTMLElement {
 	constructor() {
 		super();
 		this.listenedMedia = [];
 		this.reloadQueued = false;
 	}
 	connectedCallback() {
-		this.video = this.querySelector('video');
+		this.audio = this.querySelector('audio');
 		this.bindMediaListeners();
 	}
 	disconnectedCallback() {
@@ -22,8 +22,8 @@ class ResponsiveVideo extends HTMLElement {
 		this.querySelectorAll('source').forEach(source => {
 			if (source.media) {
 				const mqListener = () => {
-					if (source.media === this.video.currentSrc || !this.previousSiblingIsPlaying(source, this.video.currentSrc) && !this.reloadQueued) {
-						this.reloadVideo();
+					if (source.media === this.audio.currentSrc || !this.previousSiblingIsPlaying(source, this.audio.currentSrc) && !this.reloadQueued) {
+						this.reloadAudio();
 					}
 				};
 				this.listenedMedia.push({ media: source.media, handler: mqListener });
@@ -45,36 +45,36 @@ class ResponsiveVideo extends HTMLElement {
 		}
 		return false;
 	}
-	reloadVideo(){
+	reloadAudio(){
 		this.reloadQueued = true;
-		const currentTime = this.video.currentTime;
-		const playState = this.video.playState;
-		this.video.load();
-		const videoLoaded = () => {
-			this.video.playState = playState;
-			this.video.currentTime = currentTime.toString();
+		const currentTime = this.audio.currentTime;
+		const playState = this.audio.playState;
+		this.audio.load();
+		const audioLoaded = () => {
+			this.audio.playState = playState;
+			this.audio.currentTime = currentTime.toString();
 			this.reloadQueued = false;
-			this.video.removeEventListener("loadeddata", videoLoaded);
+			this.audio.removeEventListener("loadeddata", audioLoaded);
 		};
-		this.video.addEventListener("loadeddata", videoLoaded);
+		this.audio.addEventListener("loadeddata", audioLoaded);
 	}
 }
 
-// feature test for native video media switching media
-const videoMediaChangeSupport = async () => {
+// feature test for native audio media switching media
+const audioMediaChangeSupport = async () => {
 	return new Promise(resolve => {
 		const iframe = document.createElement("iframe");
-		const video = document.createElement("video");
+		const audio = document.createElement("audio");
 		const source = document.createElement("source");
 		const mediaSource = new MediaSource();
 		mediaSource.addEventListener("sourceopen", () => resolve(true));
 		source.src = URL.createObjectURL(mediaSource);
 		source.media = "(min-width:10px)";
-		video.append(source);
+		audio.append(source);
 		iframe.width = "5";
 		iframe.style.cssText = `position: absolute; visibility: hidden;`;
 		document.documentElement.append(iframe);
-		iframe.contentDocument.body.append(video);
+		iframe.contentDocument.body.append(audio);
 		setTimeout(() => { iframe.width = "15"; });
 		setTimeout(() => {
 			iframe.remove();
@@ -83,6 +83,6 @@ const videoMediaChangeSupport = async () => {
 	});
 };
 
-if( await videoMediaChangeSupport() === false ){
-	customElements.define("responsive-video", ResponsiveVideo);
+if( await audioMediaChangeSupport() === false ){
+	customElements.define("responsive-audio", ResponsiveAudio);
 }
